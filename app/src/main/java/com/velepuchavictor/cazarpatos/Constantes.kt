fb29d3bc347b5a0ee91900ab88737c47a143212d
vvelepuc <victor.velepucha@epb.edu.ec>
2022-01-17 17:53:15 -0500

    Se añade pantalla login y pantalla principal del juego

---
+++ app/src/main/java/com/velepuchavictor/cazarpatos/Constantes.kt
@@ -0,0 +1,3 @@
+package com.velepuchavictor.cazarpatos
+
+const val EXTRA_LOGIN = "EXTRA_LOGIN"
--- app/src/main/java/com/velepuchavictor/cazarpatos/MainActivity.kt
+++ app/src/main/java/com/velepuchavictor/cazarpatos/MainActivity.kt
@@ -1,11 +1,108 @@
 package com.velepuchavictor.cazarpatos
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textViewUsuario: TextView
+    lateinit var textViewContador: TextView
+    lateinit var textViewTiempo: TextView
+    lateinit var imageViewPato: ImageView
+    var contador = 0
+    var anchoPantalla = 0
+    var alturaPantalla = 0
+    var gameOver = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Inicialización de variables
+        textViewUsuario = findViewById(R.id.textViewUsuario)
+        textViewContador = findViewById(R.id.textViewContador)
+        textViewTiempo = findViewById(R.id.textViewTiempo)
+        imageViewPato = findViewById(R.id.imageViewPato)
+
+        //Obtener el usuario de pantalla login
+        val extras = intent.extras ?: return
+        val usuario = extras.getString(EXTRA_LOGIN) ?:"Unknown"
+        textViewUsuario.setText(usuario)
+
+        //Determina el ancho y largo de pantalla
+        inicializarPantalla()
+        //Cuenta regresiva del juego
+        inicializarCuentaRegresiva()
+        //Evento clic sobre la imagen del pato
+        imageViewPato.setOnClickListener {
+            if (gameOver) return@setOnClickListener
+            contador++
+            MediaPlayer.create(this, R.raw.gunshot).start()
+            textViewContador.setText(contador.toString())
+            imageViewPato.setImageResource(R.drawable.duck_clicked)
+            //Evento que se ejecuta luego de 500 milisegundos
+            Handler().postDelayed(Runnable {
+                imageViewPato.setImageResource(R.drawable.duck)
+                moverPato()
+            }, 500)
+        }
     }
+    private fun inicializarPantalla() {
+        // 1. Obtenemos el tamaño de la pantalla del dispositivo
+        val display = this.resources.displayMetrics
+        anchoPantalla = display.widthPixels
+        alturaPantalla = display.heightPixels
+    }
+
+    private fun moverPato() {
+        val minimo = imageViewPato.getWidth()/2
+        val maximoX = anchoPantalla - imageViewPato.getWidth()
+        val maximoY = alturaPantalla - imageViewPato.getHeight()
+        // Generamos 2 números aleatorios, para la coordenadas x , y
+        val randomX = Random().nextInt(maximoX - minimo ) + 1
+        val randomY = Random().nextInt(maximoY - minimo ) + 1
+
+        // Utilizamos los números aleatorios para mover el pato a esa nueva posición
+        imageViewPato.setX(randomX.toFloat())
+        imageViewPato.setY(randomY.toFloat())
+    }
+    private fun inicializarCuentaRegresiva() {
+        object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val segundosRestantes = millisUntilFinished / 1000
+                textViewTiempo.setText("${segundosRestantes}s")
+            }
+            override fun onFinish() {
+                textViewTiempo.setText("0s")
+                gameOver = true
+                mostrarDialogoGameOver()
+            }
+        }.start()
+    }
+    private fun mostrarDialogoGameOver() {
+        val builder = AlertDialog.Builder(this)
+        builder
+            .setMessage("Felicidades!!\nHas conseguido cazar $contador patos")
+            .setTitle("Fin del juego")
+            .setPositiveButton("Reiniciar",
+                { _, _ ->
+                    contador = 0
+                    gameOver = false
+                    textViewContador.setText(contador.toString())
+                    moverPato()
+                    inicializarCuentaRegresiva()
+                })
+            .setNegativeButton("Cerrar",
+                { _, _ ->
+                    //dialog.dismiss()
+                })
+        builder.create().show()
+    }
+
 }

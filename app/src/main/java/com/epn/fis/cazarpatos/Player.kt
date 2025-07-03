f1e0db1f6b27313053a8198747a261e6282b9e37
P3E009-E <P3E009-E@39681608D20P3>
2025-07-03 08:32:56 -0500

    Added RankingActivity and use of RecyclerView

---
+++ app/src/main/java/com/epn/fis/cazarpatos/Player.kt
@@ -0,0 +1,6 @@
+package com.epn.fis.cazarpatos
+
+data class Player (var username:String, var huntedDucks:Int){
+    constructor():this("",0)
+}
+
---
+++ app/src/main/java/com/epn/fis/cazarpatos/RankingActivity.kt
@@ -0,0 +1,34 @@
+package com.epn.fis.cazarpatos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ranking)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+        var jugadores = arrayListOf<Player>()
+        jugadores.add(Player("Epn.Fis",1))
+        jugadores.add(Player("Jugador2",20))
+        jugadores.add(Player("Jugador3",13))
+        jugadores.add(Player("Jugador4",19))
+        jugadores.sortByDescending { it.huntedDucks }
+
+        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+        recyclerViewRanking.adapter = RankingAdapter(jugadores);
+        recyclerViewRanking.setHasFixedSize(true);
+
+    }
+}
